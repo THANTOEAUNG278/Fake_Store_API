39970710bc2ed9d4cb7e8364890cf75eda7f2c9b
THANTOEAUNG <kyawshein034@gmail.com>
2024-06-12 10:59:13 +0900

    style::add order summary

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -6,6 +6,17 @@ import { MdDelete } from "react-icons/md";
 const Cart = () => {
   const { cart, addToCart, removeFromCart, deleteFromCart } = useContext(CartContext);
 
+  const calculateTotalItems = () => {
+    return cart.reduce((total, product) => total + product.quantity, 0);
+  };
+
+  const calculateSubtotal = () => {
+    return cart.reduce((total, product) => total + product.price * product.quantity, 0).toFixed(2);
+  };
+
+  const shippingHandling = 5.99;
+  const totalAmount = (parseFloat(calculateSubtotal()) + shippingHandling).toFixed(2);
+
   return (
     <div className="min-h-screen p-10 bg-gray-100 py-16">
       <h1 className="text-4xl font-extrabold mb-8">Your Cart</h1>
@@ -23,7 +34,7 @@ const Cart = () => {
                     <button onClick={() => removeFromCart(product.id)} className="px-3 py-1 font-bold bg-gray-300 rounded">-</button>
                     <span className="px-3 font-bold">{product.quantity}</span>
                     <button onClick={() => addToCart(product)} className="px-3 py-1 font-bold bg-gray-300 rounded">+</button>
-                    <button onClick={() => deleteFromCart(product.id)} className="px-3 py-1 bg-red-500 text-2xl mx-2 text-white rounded"><MdDelete/></button>
+                    <button onClick={() => deleteFromCart(product.id)} className="px-3 py-1 bg-red-500 text-2xl mx-2 text-white rounded"><MdDelete /></button>
                   </div>
                 </div>
               </div>
@@ -32,10 +43,24 @@ const Cart = () => {
                   <h3 className="text-lg font-bold mb-2">{product.title}</h3>
                   <p className="text-gray-500 mb-4">{product.description}</p>
                 </div>
-                
               </div>
             </div>
           ))}
+          <div className="mt-8 p-4 border-t-2">
+            <h2 className="text-2xl font-bold flex justify-end mb-4 underline">Order Summary</h2>
+            <div className="flex justify-end gap-20 mb-2">
+              <span>Items ({calculateTotalItems()}):</span>
+              <span>${calculateSubtotal()}</span>
+            </div>
+            <div className="flex justify-end gap-20 mb-2">
+              <span>Shipping & Handling:</span>
+              <span>${shippingHandling.toFixed(2)}</span>
+            </div>
+            <div className="flex justify-end gap-16 font-bold text-lg mb-4">
+              <span>Order Total:</span>
+              <span>${totalAmount}</span>
+            </div>
+          </div>
           <div className="flex justify-end mt-8">
             <button className="bg-green-500 text-white py-3 px-6 rounded-md shadow-md hover:bg-green-600 transition duration-300">Place Your Order</button>
           </div>
